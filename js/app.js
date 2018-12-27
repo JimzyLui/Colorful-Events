e1b5f358862f3f856ff5deaf05421af698089fde
Jimzy <JimzyLui@users.noreply.github.com>
2018-12-26 18:41:29 -1000

    completed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+var btnRainbowPick = document.getElementById("btnChangeColor");
+btnRainbowPick.addEventListener("click", changeColorRandom);
 
+function changeColorRandom() {
+  let arrRGB = [];
+  for (let i = 0; i < 3; i++) {
+    const val = Math.floor(Math.random() * 255 + 1);
+    arrRGB.push(val);
+  }
+  const strColor = "rgb(" + arrRGB.join(",") + ")";
+  btnRainbowPick.style.backgroundColor = strColor;
+}
